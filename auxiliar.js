74d24e9ddce7290b867084a212dc0d291967ca31
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2018-12-17 12:36:55 +0100

    Exporto metodos a auxiliar.js

--- auxiliar.js
+++ auxiliar.js
@@ -1,3 +1,7 @@
+//array de cuadrados totales y de los que quedan vacios
+var cuadrados = document.getElementsByTagName('td');
+var vacios = generaVacios(cuadrados);
+
 //MÉTODOS AUXILIARES
 function compara(nodo1,nodo2){ return nodo1.children[0].firstChild.nodeValue == nodo2.children[0].firstChild.nodeValue;}
 function vacio(nodo){return nodo.children.length == 0;}
@@ -5,3 +9,29 @@ function transformaNodo(nodo){ nodo.children[0].firstChild.nodeValue = nodo.chil
 function mueveNodo(nodo, lugar){ lugar.replaceChild(nodo.firstChild,lugar.firstChild); }
 function actualizaVacios(){vacios = generaVacios(cuadrados); }
 function actualizaLlenos(cols){return cols.filter(elem => !vacio(elem));}
+
+//detecta cuales estan vacios de entre los totales
+function generaVacios(cuadrados){
+  var vacios = [];
+  for (var i = 0; i < cuadrados.length; i++) {
+    if (cuadrados[i].childNodes.length == 1) {
+      vacios.push(cuadrados[i]);
+    }
+  }
+  return vacios;
+}
+
+//genera un 2 o 4 aleatoriamente y lo pinta entre los vacio aleatoriamente
+function generaNumero(){
+  var num = 2;
+  var aleatorio = Math.random();
+  var aleatorio1 = Math.floor(Math.random()*vacios.length);
+  if (aleatorio >= 0.7) {
+    num = 4;
+  }
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode(num));
+    p.setAttribute('class',`c${num}`);
+    vacios[aleatorio1].appendChild(p);
+    actualizaVacios();
+}
