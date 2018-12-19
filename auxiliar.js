a1c59f3d7be7ba8b42603b70956c74c395471765
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2018-12-19 02:11:17 +0100

    Extraigo gameOver()

--- auxiliar.js
+++ auxiliar.js
@@ -40,9 +40,6 @@ function generaVacios(cuadrados){
 //genera un 2 o 4 aleatoriamente y lo pinta entre los vacio aleatoriamente
 function generaNumero(){
   actualizaVacios();
-  if (vacios.length == 0) {
-      alert('GAME OVER!');
-  }else {
   var num = 2;
   var aleatorio = Math.random();
   var aleatorio1 = Math.floor(Math.random()*vacios.length);
@@ -53,7 +50,13 @@ function generaNumero(){
     p.appendChild(document.createTextNode(num));
     p.setAttribute('class',`c${num}`);
     vacios[aleatorio1].appendChild(p);
-  }
+}
+
+function gameOver(){
+    if (vacios.length == 0) {
+        alert('GAME OVER!');
+    }
+    actualizaVacios();
 }
 
 function reduce(e){
